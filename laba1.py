b7120dfc5f2d148a5e1ae070b9202958e10cf122
R09C <hodarev.ruslan@mail.ru>
2025-10-25 04:06:28 +0700

    fix segmetation

--- laba1.py
+++ laba1.py
@@ -1,5 +1,5 @@
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 import pymorphy3
 
 
@@ -10,8 +10,8 @@ with open("text.txt", "r", encoding="utf-8") as file:
 nltk.download("punkt_tab")
 m_analyzer = pymorphy3.MorphAnalyzer()
 
-word_mass_with_punctuation = word_tokenize(text)
-word_mass = []
+sentences = sent_tokenize(text, language="russian")
+
 punctuation = {
     ".",
     ",",
@@ -31,28 +31,30 @@ punctuation = {
     "...",
     "—",
 }
-for word in word_mass_with_punctuation:
-    if word not in punctuation:
-        word_mass.append(word.lower())
-
-
-# print(word_mass_with_punctuation,"\n", word_mass)
 
 return_pairs = []
 
-for i in range(len(word_mass) - 1):
-    word_1 = m_analyzer.parse(word_mass[i])[0]
-    word_1_tag = word_1.tag
-    word_2 = m_analyzer.parse(word_mass[i + 1])[0]
-    word_2_tag = word_2.tag
+for sentence in sentences:
+    word_mass_with_punctuation = word_tokenize(sentence)
+    word_mass = []
 
-    if (
-        (word_1_tag.POS in ["ADJF", "NOUN"])
-        and (word_2_tag.POS in ["ADJF", "NOUN"])
-        and (word_1_tag.case == word_2_tag.case)
-        and (word_1_tag.gender == word_2_tag.gender)
-        and (word_1_tag.number == word_2_tag.number)
-    ):
-        return_pairs.append((word_1.normal_form, word_2.normal_form))
+    for word in word_mass_with_punctuation:
+        if word not in punctuation:
+            word_mass.append(word.lower())
+
+    for i in range(len(word_mass) - 1):
+        word_1 = m_analyzer.parse(word_mass[i])[0]
+        word_1_tag = word_1.tag
+        word_2 = m_analyzer.parse(word_mass[i + 1])[0]
+        word_2_tag = word_2.tag
+
+        if (
+            (word_1_tag.POS in ["ADJF", "NOUN"])
+            and (word_2_tag.POS in ["ADJF", "NOUN"])
+            and (word_1_tag.case == word_2_tag.case)
+            and (word_1_tag.gender == word_2_tag.gender)
+            and (word_1_tag.number == word_2_tag.number)
+        ):
+            return_pairs.append((word_1.normal_form, word_2.normal_form))
 
 print(return_pairs)
